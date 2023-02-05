8a44912d383a518c4d9cd975f1c8f5516f97ddf7
Grigory_S <0064777@gmail.com>
2023-02-05 22:00:02 +0300

    less 1

--- main.py
+++ main.py
@@ -16,6 +16,4 @@ if __name__ == '__main__':
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/ """
 
 
-n = int(input('n: '))  # км за день
-m = int(input('m :'))  # маршрут
-print(m//n + )
+
